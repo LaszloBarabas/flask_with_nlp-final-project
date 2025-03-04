82496aabef6460f99d5d125e5aff01a39a8e7f4b
Laszlo Barabas <laszlo.barabas@gmail.com>
2025-03-04 16:50:13 -0500

    Added TASK 4 pre 2

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -2,10 +2,10 @@ from EmotionDetection.emotion_detection import emotion_detector
 import unittest
 
 
-class TestEmotionDetector(unittest.TestCase)
+class TestEmotionDetector(unittest.TestCase):
     def test_emotion_detector(self):
         result_1 = emotion_detector("I am glad this happened")
-        self.assertEqual(result_1['joy'], 'joy')
+        self.assertEqual(result_1['dominant_emotion'], 'joy')
 
 
 unittest.main()
